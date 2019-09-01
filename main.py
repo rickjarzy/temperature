295f5626992af6247b1ee53b7d466d74a35581ff
Paul <peru77@gmx.at>
2019-09-01 11:02:26 +0200

    update main.py

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-import Adafruit_DHT
+import adafruit_dth
+dht_sensor = adafruit_dth.DHT22
+dht_pin = 4     # GPIO Pin responsible for data transport
 
-DHT_SENSOR = Adafruit_DHT.DHT22
-DHT_PIN = 4     # GPIO Pin responsible for data connection and transfer
-
-while True:
-    hum, temp = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
-
-    if hum is not None and temp is not None:
-        print("Temperature: {0:0.1f} C° - Luftfeuchte= {1:0.1f}%".format(temp, hum))
-    else:
-        print("No Sensor data is availiable")
-
-    time.sleep(1)
+print("Programm ENDE")
